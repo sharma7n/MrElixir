703899e9e90e47ca14695a772abc870a35b9539d
sharma7n <sharma7n@gmail.com>
2017-03-10 03:53:31 +0000

    benchmarking

--- mr_v3.ex
+++ mr_v3.ex
@@ -1,9 +1,16 @@
 # v3: Distribute across map and reduce.
-# Run time is O(n / # processes) == O(chunk size)
+# If map = O(f(n)) and reduce = O(g(n)), then map |> reduce is O(f(n) + g(n))
+# MapReduce changes this to O(f(chunk_size) + g(chunk_size) + g(n / chunk_size))
 # Try changing @chunk_size and see how long it takes
 
+defmodule Benchmark do
+  def measure(function) do
+    function |> :timer.tc |> elem(0) |> Kernel./(1_000_000)
+  end
+end
+
 defmodule MapReduce do
-    @chunk_size 1
+    @chunk_size 2
     
     defp process(chunk) do
         # distributes map and reduce steps across each chunk using spawn
@@ -15,7 +22,7 @@ defmodule MapReduce do
                         x * 2 
                     end)
                 |> Enum.reduce(0, fn x, acc -> 
-                        :timer.sleep(1000) # reduce is expensive
+                        :timer.sleep(500) # reduce is expensive
                         x + acc 
                     end)
             send master, {:ok, result}
@@ -25,7 +32,8 @@ defmodule MapReduce do
     
     defp total(data) do
         # resolves calculations in the master process
-        Enum.reduce(data, 0, fn _, total -> 
+        Enum.reduce(data, 0, fn _, total ->
+            :timer.sleep(500) # total is also expensive
             receive do
                 {:ok, count} -> count + total
             end
@@ -39,5 +47,4 @@ defmodule MapReduce do
     end
 end
 
-result = MapReduce.run 1..50
-IO.inspect result
+IO.inspect Benchmark.measure(fn -> MapReduce.run 1..10 end)
